7eb08ef3eabb73fbdc7e4f549bd7f9ef744a6d37
tkjacksonhod <tkjacksonhod@gmail.com>
2019-11-02 15:52:11 -0500

    Update debugLogicalOperatorDrills-1.py

--- debugLogicalOperatorDrills-1.py
+++ debugLogicalOperatorDrills-1.py
@@ -24,32 +24,37 @@ START HERE
 # )Broken: Change the code so it returns True
 # )4 !== 4 and 3 == 3 
 # )Correct:
-
+4 == 4 and 3 == 3
 #2)
 # )Broken: Change the code so it returns True
 # )3**2 == 3+2 or 1+2 <= 3 and !True
 # )Correct:
-
+3**2 == 3+2 or 1+2 <= 3 
 #3)
 # )Broken: Change it so it returns False
 # )(True and False) or (True or false)
 # )Correct:
-
+(True == False) or (False == True)
 #4)
 # )Broken: Make the code return True
 # )((5==5) and 4<3) and not(True and 8**2 >= 64)
 # )Correct:
-
+(5 == 5 and 4<3) and (8**2 >= 64)
 #5)
 # )Broken:Make the code return False
 # )a = True
 # )b = False
 # )((a or b) and a) or (a or b)
 # )Correct:
-
+a = True
+b = False
+(a == b) and a == True 
 #6)
 # )Broken: make the code return True
 # )a = True
 # )b = False
 # )not(a or b) and a
 # )Correct:
+a = True
+b = False
+not(a == b) and (a == True)
